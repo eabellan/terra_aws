807e230638ede46e9eafec7d81930fdb8e27ee29
edu <eabellan@cafdigitalservices.com>
2024-05-09 11:38:17 +0200

    This is my 1st commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {}
+
+resource "aws_security_group" "my_sg" {
+  name = "openallEdu"
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "MyEc2" {
+  ami                    = "ami-07caf09b362be10b8"
+  instance_type          = "t2.nano"
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+
+  tags = {
+    Name = "EduTest"
+  }
+}
